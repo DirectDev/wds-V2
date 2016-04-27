42064ae44dff3ab9f3ed120005e6d815b6c505ad
DirectDev <serviceclient@directdev.fr>
2016-04-27 11:51:11 +0200

    test facebook event

---
+++ src/Front/FrontBundle/Controller/FacebookController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Front\FrontBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class FacebookController extends Controller {
+
+    public function testAction(Request $request) {
+
+        $em = $this->getDoctrine()->getManager();
+        $session = $this->getRequest()->getSession();
+        
+        // besoin de se connecter avec un compte fb d'abord
+        
+        $facebookServices = $this->get('facebook.services');
+        $facebookServices->importEvent('1599833447000034');
+        
+        return new Response('test facebook', 200);
+    }
+
+}
--- src/Front/FrontBundle/Services/FacebookServices.php
+++ src/Front/FrontBundle/Services/FacebookServices.php
@@ -9,48 +9,70 @@ use Facebook\FacebookRequestException;
 
 class FacebookServices {
 
-//    private $facebookSession;
-//    private $user;
-//
-//    public function __construct($em, $securityContext, $facebook_app_id, $facebook_app_secret) {
-//        $user = $securityContext->getToken()->getUser();
-//        
-//        FacebookSession::setDefaultApplication($facebook_app_id, $facebook_app_secret);
-//        $this->facebookSession = new FacebookSession($user->getFacebookAccessToken());
-//    }
-//
-//    public function importEvents() {
-//        try {
-//            
-//            $me = (new FacebookRequest( $this->facebookSession, 'GET', '/me'))
-//                    ->execute()
-//                    ->getGraphObject(GraphUser::className());
-//            echo $me->getName();
-//            
-//            
-//            $me = (new FacebookRequest( $this->facebookSession, 'GET', '/me/events'))
-//                    ->execute()
-//                    ->getGraphObjectList();
-//            var_dump($me);
-//            
-//            $request = (new FacebookRequest($this->facebookSession, 'GET', '/me/events'));
-//            $response = $request->execute();
-////            var_dump($response);
-//            $graphObject = $response->getGraphObject();
-//            var_dump ($graphObject);
-//            
-//            $request = (new FacebookRequest($this->facebookSession, 'GET', '/me'));
-//            $response = $request->execute();
-//            $graphObject = $response->getGraphObject();
-//            var_dump ($graphObject);
-//            
-//            
-//        } catch (FacebookRequestException $e) {
-//            // The Graph API returned an error
-//        } catch (\Exception $e) {
-//            // Some other error occurred
-//        }
-//    }
+    private $facebook;
+    private $user;
+    private $access_token;
+
+    public function __construct($em, $securityContext, $facebook_app_id, $facebook_app_secret) {
+        $this->user = $securityContext->getToken()->getUser();
+        if ($this->user)
+            $this->access_token = $this->user->getFacebookAccessToken();
+
+        $this->facebook = new \Facebook\Facebook([
+            'app_id' => $facebook_app_id,
+            'app_secret' => $facebook_app_secret,
+            'default_graph_version' => 'v2.6',
+        ]);
+        $this->facebook->setDefaultAccessToken((string) $this->access_token);
+    }
+
+    public function importEvents() {
+        
+    }
+
+    public function importEvent($facebook_event_id = null) {
+        if (!$facebook_event_id)
+            return;
+        /*
+         * Node
+         */
+
+
+        $request = $this->facebook->request('GET', '/' . $facebook_event_id);
+        try {
+            $response = $this->facebook->getClient()->sendRequest($request);
+        } catch (\Facebook\Exceptions\FacebookResponseException $e) {
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch (\Facebook\Exceptions\FacebookSDKException $e) {
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
+
+        $event_node = $response->getGraphNode();
+//        var_dump($event_node);
+//        var_dump($event_node->getField('id'));
+
+        $place_node = $event_node->getField('place');
+//        var_dump($place_node);
+        $address_node = $place_node->getField('location');
+//        var_dump($address_node);
+
+
+
+        /*
+         * 
+         * Edges
+         */
+        $request = $this->facebook->request('GET', '/' . $facebook_event_id . '/admins');
+        $response = $this->facebook->getClient()->sendRequest($request);
+
+
+        $event_admins_edge = $response->getGraphEdge();
+        var_dump($event_admins_edge);
+
+        var_dump($response);
+    }
 
 }
 
